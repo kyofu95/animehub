8c2f0337e86fa59638f2ece453509417708eb7c2
sny <127958265+kyofu95@users.noreply.github.com>
2024-12-28 15:52:52 +0300

    attach anime and user to root router

--- app/api/api.py
+++ app/api/api.py
@@ -1,7 +1,11 @@
 from fastapi import APIRouter
 
+from .endpoints.user import router as user_router
 from .endpoints.auth import router as auth_router
+from .endpoints.anime import router as anime_router
 
 api_router = APIRouter(prefix="/api")
 
+api_router.include_router(user_router)
 api_router.include_router(auth_router)
+api_router.include_router(anime_router)
