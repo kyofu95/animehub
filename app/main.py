55931ff7fc2041de579472c8e05bf61cdd53c0e0
sny <127958265+kyofu95@users.noreply.github.com>
2025-01-23 17:43:30 +0300

    install new exception handlers

--- app/main.py
+++ app/main.py
@@ -1,10 +1,12 @@
 from contextlib import asynccontextmanager
 from typing import AsyncIterator
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Request, status
+from fastapi.responses import JSONResponse
 from fastapi_pagination import add_pagination
 
 from app.api.api import api_router
+from app.core.exceptions import AlreadyExistsError, DatabaseError, NotFoundError
 from app.database.orm import start_mapper
 
 
@@ -28,6 +30,36 @@ async def lifespan(fastapi: FastAPI) -> AsyncIterator[None]:
     yield
 
 
+def install_exception_handlers(fast_app: FastAPI) -> None:
+    """
+    Install custom exception handlers for the FastAPI application.
+    This function registers exception handlers for specific application-level errors.
+    These handlers capture exceptions raised during the request lifecycle and return
+    appropriate HTTP responses with error messages.
+
+    Args:
+        fast_app (FastAPI): The FastAPI application instance.
+    """
+
+    @fast_app.exception_handler(NotFoundError)
+    async def not_found_exc(request: Request, exc: NotFoundError):
+        return JSONResponse(content={"detail": str(exc)}, status_code=status.HTTP_404_NOT_FOUND)
+
+    @fast_app.exception_handler(AlreadyExistsError)
+    async def already_exists_exc(request: Request, exc: AlreadyExistsError):
+        return JSONResponse(
+            content={"detail": str(exc)},
+            status_code=status.HTTP_409_CONFLICT,
+        )
+
+    @fast_app.exception_handler(DatabaseError)
+    async def database_exc(request: Request, exc: DatabaseError):
+        return JSONResponse(
+            content={"detail": str(exc)},
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+        )
+
+
 def create_app() -> FastAPI:
     """
     Create and configure the FastAPI application.
@@ -42,6 +74,8 @@ def create_app() -> FastAPI:
     api = FastAPI(lifespan=lifespan)
 
     api.include_router(api_router)
+
+    install_exception_handlers(api)
 
     add_pagination(api)
 
