068c87f5b5f82e22adc4eb513a9fe2659a2011de
sny <127958265+kyofu95@users.noreply.github.com>
2025-01-31 15:46:55 +0300

    attach watchlist endpoints to the root router

--- app/api/api.py
+++ app/api/api.py
@@ -3,9 +3,11 @@ from fastapi import APIRouter
 from .endpoints.user import router as user_router
 from .endpoints.auth import router as auth_router
 from .endpoints.anime import router as anime_router
+from .endpoints.watchlist import router as watchentry_router
 
 api_router = APIRouter(prefix="/api")
 
 api_router.include_router(user_router)
 api_router.include_router(auth_router)
 api_router.include_router(anime_router)
+api_router.include_router(watchentry_router)
