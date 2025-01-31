77656220e6d4850c6f7ec7ae0255940dc65a33c8
sny <127958265+kyofu95@users.noreply.github.com>
2025-01-31 15:45:16 +0300

    bash script proper if/else

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,9 +4,10 @@
 alembic upgrade head
 
 if [[ "$DEBUG" == "true" ]]; then
+    # Try to install debugger and run server with it
     pip install debugpy
     python3 -m debugpy --listen 0.0.0.0:5678 -m uvicorn app.main:app --host 0.0.0.0 --port 8000
+else
+    # Run the server
+    uvicorn app.main:app --host 0.0.0.0 --port 8000
 fi
-
-# Run the API
-uvicorn app.main:app --host 0.0.0.0 --port 8000
