771c1434fffb1697a9f88c132ed95966e5ea581e
sny <127958265+kyofu95@users.noreply.github.com>
2025-02-09 15:03:58 +0300

    connect health api to root router

--- app/api/api.py
+++ app/api/api.py
@@ -1,8 +1,9 @@
 from fastapi import APIRouter
 
-from .endpoints.user import router as user_router
-from .endpoints.auth import router as auth_router
 from .endpoints.anime import router as anime_router
+from .endpoints.auth import router as auth_router
+from .endpoints.health import router as health_router
+from .endpoints.user import router as user_router
 from .endpoints.watchlist import router as watchentry_router
 
 api_router = APIRouter(prefix="/api")
@@ -11,3 +12,4 @@ api_router.include_router(user_router)
 api_router.include_router(auth_router)
 api_router.include_router(anime_router)
 api_router.include_router(watchentry_router)
+api_router.include_router(health_router)
