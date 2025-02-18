4eb6063aefd3545745b173dc07f2b856bc29654c
sny <127958265+kyofu95@users.noreply.github.com>
2025-02-18 18:06:03 +0300

    update oauth routines with refresh token

--- app/api/endpoints/utils/oauth.py
+++ app/api/endpoints/utils/oauth.py
@@ -3,23 +3,27 @@ from typing import Annotated
 from fastapi import Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer
 
+from app.core.exceptions import TokenError
 from app.entity.user import User
 
 from .di_deps import UserServiceDep
-from .jwt import TokenError, decode_access_token
+from .jwt import decode_access_token, decode_refresh_token
 
 oauth_scheme = OAuth2PasswordBearer(tokenUrl="/api/token")
 
 AccessToken = Annotated[str, Depends(oauth_scheme)]
 """The OAuth2 access token extracted from the request."""
 
+RefreshToken = Annotated[str, Depends(oauth_scheme)]
+"""The OAuth2 refresh token extracted from the request."""
 
-async def get_user_from_token(token: str, user_service: UserServiceDep) -> User:
+
+async def get_user_from_token(token: str, user_service: UserServiceDep, token_type: str) -> User:
     """
-    Retrieves a user from the given access token.
+    Retrieves a user from the given token.
 
     Args:
-        token (str): The JWT access token provided by the user.
+        token (str): The JWT access or refresh token provided by the user.
         user_service (UserServiceDep): The user service dependency for accessing user data.
 
     Returns:
@@ -29,8 +33,15 @@ async def get_user_from_token(token: str, user_service: UserServiceDep) -> User:
         HTTPException: If the token is invalid, expired, or if the user cannot be found.
     """
 
+    assert token_type in ["access", "refresh"]
+
+    if token_type == "access":
+        decode_func = decode_access_token
+    else:
+        decode_func = decode_refresh_token
+
     try:
-        user_id = decode_access_token(token)
+        user_id = decode_func(token)
     except TokenError as exc:
         raise HTTPException(
             status_code=status.HTTP_400_BAD_REQUEST, detail=str(exc), headers={"WWW-Authenticate": "Bearer"}
@@ -45,7 +56,7 @@ async def get_user_from_token(token: str, user_service: UserServiceDep) -> User:
     return user
 
 
-async def get_current_user(token: AccessToken, user_service: UserServiceDep) -> User:
+async def get_current_user_from_access_token(token: AccessToken, user_service: UserServiceDep) -> User:
     """
     Retrieves the currently authenticated user based on the access token.
 
@@ -63,8 +74,31 @@ async def get_current_user(token: AccessToken, user_service: UserServiceDep) -> 
     if not token:
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Not authorized.")
 
-    return await get_user_from_token(token, user_service)
+    return await get_user_from_token(token, user_service, "access")
 
 
-CurrentUser = Annotated[User, Depends(get_current_user)]
+async def get_current_user_from_refresh_token(token: RefreshToken, user_service: UserServiceDep) -> User:
+    """
+    Retrieves the currently authenticated user based on the refresh token.
+
+    Args:
+        token (RefreshToken): The access token obtained via OAuth2PasswordBearer.
+        user_service (UserServiceDep): The user service dependency for accessing user data.
+
+    Returns:
+        User: The currently authenticated user.
+
+    Raises:
+        HTTPException: If the token is missing or the user cannot be authenticated.
+    """
+    if not token:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Not authorized.")
+
+    return await get_user_from_token(token, user_service, "refresh")
+
+
+CurrentUser = Annotated[User, Depends(get_current_user_from_access_token)]
+"""Currently authenticated user, automatically resolved via dependency."""
+
+CurrentUserFromRefresh = Annotated[User, Depends(get_current_user_from_refresh_token)]
 """Currently authenticated user, automatically resolved via dependency."""
