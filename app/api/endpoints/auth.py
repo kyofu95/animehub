be8fc5f477042b403029a57d3376b857f7c6edda
sny <127958265+kyofu95@users.noreply.github.com>
2025-02-19 18:30:54 +0300

    make single token endpoint, and make it work with refresh tokens

--- app/api/endpoints/auth.py
+++ app/api/endpoints/auth.py
@@ -1,17 +1,17 @@
 from typing import Annotated
 
 from fastapi import APIRouter, Depends, HTTPException, status
-from fastapi.security import OAuth2PasswordRequestForm
 
 from .schemes.token import TokenResponse
-from .utils.jwt import encode_access_token, encode_refresh_token
 from .utils.di_deps import UserServiceDep
-from .utils.oauth import CurrentUserFromRefresh
+from .utils.jwt import encode_token
+from .utils.oauth import get_current_user_from_refresh_token
+from .utils.refresh_request_form import OAuth2PasswordAndRefreshRequestForm
 
 router = APIRouter(prefix="/token", tags=["Auth"])
 
 
-FormData = Annotated[OAuth2PasswordRequestForm, Depends()]
+FormData = Annotated[OAuth2PasswordAndRefreshRequestForm, Depends()]
 
 
 @router.post("/", response_model=TokenResponse, status_code=status.HTTP_200_OK)
@@ -33,7 +33,11 @@ async def get_tokens(user_data: FormData, service: UserServiceDep) -> TokenRespo
         TokenResponse: A response containing the access token, refresh token, and token type.
     """
 
-    user = await service.get_by_login_auth(user_data.username, user_data.password)
+    if user_data.grant_type == "refresh_token":
+        user = await get_current_user_from_refresh_token(token=user_data.refresh_token, user_service=service)
+    else:
+        user = await service.get_by_login_auth(user_data.username, user_data.password) # type: ignore
+
     if not user:
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
@@ -41,34 +45,7 @@ async def get_tokens(user_data: FormData, service: UserServiceDep) -> TokenRespo
             headers={"WWW-Authenticate": "Bearer"},
         )
 
-    access_token = encode_access_token(user.id)
-    refresh_token = encode_refresh_token(user.id)
+    access_token = encode_token(user.id, "access")
+    refresh_token = encode_token(user.id, "refresh")
 
-    return TokenResponse(access_token=access_token, refesh_token=refresh_token, token_type="bearer")
-
-
-@router.post("/refresh", response_model=TokenResponse, status_code=status.HTTP_200_OK)
-async def refresh_tokens(user: CurrentUserFromRefresh) -> TokenResponse:
-    """
-    Refresh the access token using a valid refresh token.
-
-    This endpoint generates a new access token and refresh token if the provided refresh
-    token is valid.
-
-    Args:
-        user (CurrentUserFromRefresh): The currently authenticated user from the refresh token.
-
-    Raises:
-        HTTPException(401): If the refresh token is invalid.
-
-    Returns:
-        TokenResponse: A response containing the new access token, refresh token, and token type.
-    """
-
-    if not user:
-        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid refresh token")
-
-    access_token = encode_access_token(user.id)
-    refresh_token = encode_refresh_token(user.id)
-
-    return TokenResponse(access_token=access_token, refesh_token=refresh_token, token_type="bearer")
+    return TokenResponse(access_token=access_token, refresh_token=refresh_token, token_type="bearer")
