e4d23b59d16d0ac8a3c1820f404721665d624c61
sny <127958265+kyofu95@users.noreply.github.com>
2025-02-19 18:27:02 +0300

    corresponding jwt tests

--- tests/test_jwt.py
+++ tests/test_jwt.py
@@ -7,38 +7,27 @@ from uuid import uuid4
 import pytest
 from jwt import encode as real_jwt_encode
 
-from app.api.endpoints.utils.jwt import (
-    decode_access_token,
-    decode_refresh_token,
-    encode_access_token,
-    encode_refresh_token,
-)
+from app.api.endpoints.utils.jwt import decode_token, encode_token
 from app.core.config import jwt_settings
 from app.core.exceptions import TokenError
 
 
 @pytest.mark.asyncio
-async def test_proper_jwt_access():
+async def test_proper_jwt():
 
     id_ = uuid4()
 
-    token = encode_access_token(id_)
+    token = encode_token(id_, "access")
     assert token
 
-    decoded_id = decode_access_token(token)
+    decoded_id = decode_token(token, "access")
 
     assert decoded_id == id_
 
-
-@pytest.mark.asyncio
-async def test_proper_jwt_refresh():
-
-    id_ = uuid4()
-
-    token = encode_refresh_token(id_)
+    token = encode_token(id_, "refresh")
     assert token
 
-    decoded_id = decode_refresh_token(token)
+    decoded_id = decode_token(token, "refresh")
 
     assert decoded_id == id_
 
@@ -47,13 +36,13 @@ async def test_proper_jwt_refresh():
 async def test_jwt_bad_tokens_access():
 
     with pytest.raises(TokenError) as exc_info:
-        decode_access_token("")
+        decode_token("", "access")
 
     assert exc_info.type is TokenError
     assert exc_info.value.args[0] == "Invalid token"
 
     with pytest.raises(TokenError) as exc_info:
-        decode_access_token("bad_hash")
+        decode_token("bad_hash", "access")
 
     assert exc_info.type is TokenError
     assert exc_info.value.args[0] == "Invalid access token"
@@ -63,13 +52,13 @@ async def test_jwt_bad_tokens_access():
 async def test_jwt_bad_tokens_refresh():
 
     with pytest.raises(TokenError) as exc_info:
-        decode_refresh_token("")
+        decode_token("", "refresh")
 
     assert exc_info.type is TokenError
     assert exc_info.value.args[0] == "Invalid token"
 
     with pytest.raises(TokenError) as exc_info:
-        decode_refresh_token("bad_hash")
+        decode_token("bad_hash", "refresh")
 
     assert exc_info.type is TokenError
     assert exc_info.value.args[0] == "Invalid refresh token"
@@ -94,11 +83,11 @@ async def test_jwt_expired_signature_access(monkeypatch):
 
     monkeypatch.setattr("app.api.endpoints.utils.jwt.jwt_encode", patched_encode)
 
-    token = encode_access_token(id_)
+    token = encode_token(id_, "access")
     assert token
 
     with pytest.raises(TokenError) as exc_info:
-        decode_access_token(token)
+        decode_token(token, "access")
 
     assert exc_info.type is TokenError
     assert exc_info.value.args[0] == "Expired access token signature"
@@ -123,11 +112,11 @@ async def test_jwt_expired_signature_refresh(monkeypatch):
 
     monkeypatch.setattr("app.api.endpoints.utils.jwt.jwt_encode", patched_encode)
 
-    token = encode_access_token(id_)
+    token = encode_token(id_, "refresh")
     assert token
 
     with pytest.raises(TokenError) as exc_info:
-        decode_refresh_token(token)
+        decode_token(token, "refresh")
 
     assert exc_info.type is TokenError
     assert exc_info.value.args[0] == "Expired refresh token signature"
@@ -143,6 +132,7 @@ async def test_jwt_invalid_payload_access(monkeypatch):
     invalid_payload = {
         "exp": datetime.now(timezone.utc) + delta,
         "iat": datetime.now(timezone.utc),
+        "type": "access"
     }
 
     def patched_encode(payload, key, algorithm):
@@ -151,11 +141,11 @@ async def test_jwt_invalid_payload_access(monkeypatch):
 
     monkeypatch.setattr("app.api.endpoints.utils.jwt.jwt_encode", patched_encode)
 
-    token = encode_access_token(id_)
+    token = encode_token(id_, "access")
     assert token
 
     with pytest.raises(TokenError) as exc_info:
-        decode_access_token(token)
+        decode_token(token, "access")
 
     assert exc_info.type is TokenError
     assert exc_info.value.args[0] == "Invalid access token payload"
@@ -171,6 +161,7 @@ async def test_jwt_invalid_payload_refresh(monkeypatch):
     invalid_payload = {
         "exp": datetime.now(timezone.utc) + delta,
         "iat": datetime.now(timezone.utc),
+        "type": "refresh"
     }
 
     def patched_encode(payload, key, algorithm):
@@ -179,11 +170,11 @@ async def test_jwt_invalid_payload_refresh(monkeypatch):
 
     monkeypatch.setattr("app.api.endpoints.utils.jwt.jwt_encode", patched_encode)
 
-    token = encode_refresh_token(id_)
+    token = encode_token(id_, "refresh")
     assert token
 
     with pytest.raises(TokenError) as exc_info:
-        decode_refresh_token(token)
+        decode_token(token, "refresh")
 
     assert exc_info.type is TokenError
     assert exc_info.value.args[0] == "Invalid refresh token payload"
